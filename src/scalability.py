9106831e7e7dbb58e30061f51bf1448e95d6af4e
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2019-01-30 18:49:24 +0100

    updated scalability test

--- src/scalability.py
+++ src/scalability.py
@@ -17,7 +17,7 @@ for t in trees:
     patterns = int(t * pattern_factor)
     avg_length = 10
     times = []
-    for i in range(10):
+    for i in range(5):
         start = time.time()
         generator = TransactionGenerator(t, patterns, avg_length, 10, 100, 5)
         g = generator.generate_data()
