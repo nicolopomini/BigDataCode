a84b220281e3fb3a7fc8fdcce6b41f5c3b1b0e18
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2019-01-30 18:48:51 +0100

    updated scalability test

--- src/scalability.py
+++ src/scalability.py
@@ -8,24 +8,28 @@ from logic.generator import TransactionGenerator
 mins = []
 maxs = []
 avgs = []
-
-trees = [10, 100, 1000, 10000, 100000]
+# change color in graph!!
+# use "g" for factor = 0.5, "b" for factor = "0.8"
+trees = [10, 100, 1000]
+pattern_factor = 0.2
+# try with 0.5, 0.8
 for t in trees:
-    patterns = t
+    patterns = int(t * pattern_factor)
     avg_length = 10
     times = []
-    for _ in range(10):
+    for i in range(1):
         start = time.time()
         generator = TransactionGenerator(t, patterns, avg_length, 10, 100, 5)
         g = generator.generate_data()
         finish = time.time()
         times.append(finish - start)
+        print("Done dimension %d, iteration %d" % (t, i))
     mins.append(min(times))
     maxs.append(max(times))
     avgs.append(sum(times) / len(times))
 
 plt.figure()
-plt.title("#patterns = #transactions")
+plt.title("#patterns = %f * #transactions" % pattern_factor)
 plt.xlabel("Total transactions")
 plt.ylabel("Time [s]")
 plt.grid()
